2264808111b4d757cdb2ca10b511bfd1f0445399
tkinc004 <122325375+tkinc004@users.noreply.github.com>
2025-10-28 12:16:29 +0000

    Implement command-line arguments for user data
    The script takes server, user, and password as command-line arguments and prints them.

---
+++ base_de_dados.py
@@ -0,0 +1,9 @@
+Deve receber o servidor, utilizador e password passados por argumento e imprimir os dados
+import sys
+v1= sys.argv[1]
+v2= sys.argv[2]
+v3=sys.argv[3]
+print("Os dados do utilizador são:")
+print("O servidor é v1")
+print("O utilizador é v2")
+print("A palavra passe é v3")
